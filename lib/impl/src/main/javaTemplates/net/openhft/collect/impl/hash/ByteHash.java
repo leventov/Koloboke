leventov/Koloboke

commit c219bfcdbaab94baad8b1358299b9f39ec64a0b6

#23: Implemented parallel layout of maps keys and values, many fixes, refactorings and improvements to JPSG and lib/impl-generator by the way

lib/impl/src/main/javaTemplates/net/openhft/collect/impl/hash/ByteHash.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* with byte|char|short|int|long elem */
+
+package net.openhft.collect.impl.hash;
+
+
+interface ByteHash extends Hash {
+
+    byte freeValue();
+
+    boolean supportRemoved();
+
+    /**
+     * @throws java.lang.UnsupportedOperationException
+     */
+    byte removedValue();
+}

commit a674283c22291435f602fa04bc725b9fabffdb40

Fixed a templating bug introduced in c219bfc. Strange how it hadn't reveal itself for so long time.

lib/impl/src/main/javaTemplates/net/openhft/collect/impl/hash/ByteHash.java
@@ -1,3 +1,4 @@
+/* with byte|char|short|int|long elem */
 /*
  * Copyright 2014 the original author or authors.
  *