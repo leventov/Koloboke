leventov/Koloboke

commit dd96166eeff5ecc9daabcc4d82320d673ae3dbf1

Implemented LHash sets and maps. LHash/DHash switch depending on HashConfig properties. A number of minor fixes by the way.

buildSrc/src/main/groovy/net/openhft/jpsg/collect/algo/hash/HashMapQueryUpdateMethodGenerator.java
@@ -53,6 +53,8 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
     // if values copy should be lifted from branches
     boolean commonValuesCopy;
 
+    boolean commonCapacityMaskCopy = false;
+
     @Override
     public String defaultValue() {
         if (cxt.isObjectValue() || cxt.genericVersion()) return "null";
@@ -79,7 +81,7 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
             if (removedSlot) {
                 lines("postRemovedSlotInsertHook();");
             } else {
-                lines("postFreeSlotInsertHook();");
+                lines(isLHash(cxt) ? "postInsertHook();" : "postFreeSlotInsertHook();");
             }
         } else {
             lines("insertAt(insertionIndex, " + unwrappedKey() + ", " + value + ");");
@@ -244,7 +246,7 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
     private void generateAbsent(boolean removedSlot, boolean replaceValues) {
         String time = method.baseOp() == INSERT && !method.inline() ? "was" : "is";
         String comment = "// key " + time + " absent";
-        if (cxt.mutable() && method.inline()) {
+        if (cxt.mutable() && method.inline() && !isLHash(cxt)) {
             comment += removedSlot ? ", removed slot" : ", free slot";
         }
         lines(comment);
@@ -341,13 +343,16 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
         return valUsages;
     }
 
-    private int countValuesUsages(int branchStart) {
-        int valArrayUsages = 0;
-        for (int i = branchStart; i < lines.size(); i++) {
-            String line = lines.get(i);
-            valArrayUsages += countOccurrences(line, VALUES_SUB);
+    private int countUsages(int fromLine, String s) {
+        int usages = 0;
+        for (int i = fromLine; i < lines.size(); i++) {
+            usages += countOccurrences(lines.get(i), s);
         }
-        return valArrayUsages;
+        return usages;
+    }
+
+    private int countValuesUsages(int branchStart) {
+        return countUsages(branchStart, VALUES_SUB);
     }
 
     String removedValue() {
@@ -382,22 +387,32 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
 
     @Override
     public MethodGenerator remove() {
-        incrementModCount();
-        String keys;
-        if (method.inline()) {
-            keys = "keys";
-            if (cxt.isObjectKey()) {
-                keys = "((Object[]) " + keys + ")";
-            }
-        } else {
-            keys = "set";
-        }
-        lines(keys + "[index] = " + removedValue() + ";");
-        if (cxt.isObjectValue()) {
-            lines(values() + "[index] = null;");
-        }
-        lines("postRemoveHook();");
         permissions.add(REMOVE);
+        if (isLHash(cxt) && !method.removeIsHighlyProbable()) {
+            lines("removeAt(index);");
+        } else if (isLHash(cxt)) {
+            if (!method.inline() || !commonCapacityMaskCopy)
+                lines("int capacityMask = keys.length - 1;");
+            if (method.inline())
+                commonCapacityMaskCopy = true;
+            new ShiftRemove(this, cxt, VALUES_SUB).generate();
+        } else {
+            incrementModCount();
+            String keys;
+            if (method.inline()) {
+                keys = "keys";
+                if (cxt.isObjectOrNullKey()) {
+                    keys = "((Object[]) " + keys + ")";
+                }
+            } else {
+                keys = "set";
+            }
+            lines(keys + "[index] = " + removedValue() + ";");
+            if (cxt.isObjectValue()) {
+                lines(values() + "[index] = null;");
+            }
+            lines("postRemoveHook();");
+        }
         return this;
     }
 
@@ -412,7 +427,7 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
     private void generateInline() {
         inlineBeginning();
         inlineLocals();
-        String curAssignment = curAssignment(cxt, "keys", unwrappedKey(), false);
+        String curAssignment = curAssignment(cxt, "keys", unwrappedKey(), commonCapacityMaskCopy);
         if (method.mostProbableBranch() == KEY_ABSENT) {
             firstIndexFreeCheck(curAssignment);
             if (separatePresent) {
@@ -468,11 +483,11 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
     }
 
     private void innerInline() {
-        if (cxt.mutable() && (cxt.isObjectKey() || method.baseOp() != GET)) {
+        if (possibleRemovedSlots(cxt) && (cxt.isObjectKey() || method.baseOp() != GET)) {
             if (method.baseOp() != GET)
                 lines("int firstRemoved;");
             if (!cxt.isObjectKey()) {
-                countStep();
+                computeStep(this, cxt);
             }
             ifBlock(isNotRemoved(cxt, "cur"));
             if (cxt.isObjectKey()) {
@@ -486,7 +501,7 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
             }
             ifBlock("noRemoved()");
             if (cxt.isObjectKey())
-                countStep();
+                computeStep(this, cxt);
             keySearchLoop(true);
             if (method.baseOp() != GET) {
                 elseBlock();
@@ -506,27 +521,37 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
             }
             blockEnd();
             if (cxt.isObjectKey())
-                countStep();
+                computeStep(this, cxt);
             if (method.baseOp() == GET) {
                 keySearchLoop(false);
             } else {
                 keySearchLoopDifferentRemovedHandling();
             }
         } else {
-            countStep();
+            if (isLHash(cxt) && cxt.isNullKey() && !commonCapacityMaskCopy)
+                lines("capacityMask = keys.length - 1;");
+            computeStep(this, cxt);
             keySearchLoop(true);
         }
     }
 
     private void inlineLocals() {
-        if (cxt.isObjectKey())
-            lines("// noinspection unchecked");
-        lines(cxt.keyUnwrappedType() + "[] keys = " +
-                        (cxt.isObjectKey() ? "(" + cxt.keyUnwrappedType() + "[]) " : "") + "set;");
+        copyUnwrappedKeys(this, cxt);
         if (commonValuesCopy)
             copyValues();
+        String locals;
+        if (isLHash(cxt)) {
+            if (commonCapacityMaskCopy) {
+                lines("int capacityMask = keys.length - 1;");
+                locals = "";
+            } else {
+                locals = "capacityMask, ";
+            }
+        } else {
+            locals = (!cxt.isNullKey() ? "capacity, hash, " : "");
+        }
         lines(
-                "int " + (cxt.isNullKey() ? "" : "capacity, hash, ") + "index;",
+                "int " + locals + "index;",
                 cxt.keyUnwrappedType() + " cur;"
         );
     }
@@ -558,14 +583,15 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
 
                 String removed = isRemoveOp ? "(removed = removedValue)" : "removedValue";
                 ifBlock(unwrappedKey() + " != (free = freeValue)" +
-                        (cxt.mutable() ? " && " + unwrappedKey() + " != " + removed : ""));
+                        (possibleRemovedSlots(cxt) ? " && " + unwrappedKey() + " != " + removed :
+                                ""));
             } else {
                 lines(cxt.keyType() + " free;");
-                if (cxt.mutable())
+                if (possibleRemovedSlots(cxt))
                     lines(cxt.keyType() + " removed = removedValue;");
                 ifBlock(unwrappedKey() + " == (free = freeValue)");
                 lines("free = changeFree();");
-                if (cxt.mutable()) {
+                if (possibleRemovedSlots(cxt)) {
                     elseIf(unwrappedKey() + " == removed");
                     lines("removed = changeRemoved();");
                 }
@@ -605,15 +631,9 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
         }
     }
 
-    private void countStep() {
-        if (!cxt.isNullKey()) {
-            lines(step());
-        }
-    }
-
     private void keySearchLoop(boolean noRemoved) {
         lines("while (true)").block();
-        nextIndex();
+        nextIndex(this, cxt);
         if (method.mostProbableBranch() == KEY_PRESENT) {
             ifBlock("(cur = keys[index]) == " + unwrappedKey());
             generateOrGoToPresent();
@@ -639,17 +659,10 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
         blockEnd();
     }
 
-    private void nextIndex() {
-        if (!cxt.isNullKey()) {
-            lines(HashMethodGeneratorCommons.nextIndex());
-        } else {
-            lines("index++;");
-        }
-    }
 
     private void keySearchLoopDifferentRemovedHandling() {
         lines("while (true)").block();
-        nextIndex();
+        nextIndex(this, cxt);
         if (method.mostProbableBranch() == KEY_PRESENT) {
             ifBlock("(cur = keys[index]) == " + unwrappedKey());
             generateOrGoToPresent();
@@ -684,7 +697,7 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
     }
 
     private String objectKeyEqualsCond(boolean noRemoved) {
-        return (cxt.mutable() && !noRemoved ? "cur != REMOVED && " : "") +
+        return (possibleRemovedSlots(cxt) && !noRemoved ? "cur != REMOVED && " : "") +
                 "keyEquals(" + unwrappedKey() + ", cur)";
     }
 
@@ -723,7 +736,7 @@ public class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMethodGener
     }
 
     private String absentLabel() {
-        if (method.baseOp() != GET && cxt.mutable()) {
+        if (method.baseOp() != GET && possibleRemovedSlots(cxt)) {
             return "keyAbsentFreeSlot";
         } else {
             return "keyAbsent";

.idea/encodings.xml
@@ -2,3 +2,4 @@
 <project version="4">
   <component name="Encoding" useUTFGuessing="true" native2AsciiForPropertiesFiles="false" />
 </project>
+

.idea/misc.xml
@@ -7,3 +7,4 @@
     <output url="file://$PROJECT_DIR$/out" />
   </component>
 </project>
+

buildSrc/src/main/groovy/net/openhft/jpsg/collect/MethodContext.java
@@ -81,6 +81,10 @@ public class MethodContext {
         return NULL.equals(keyOption());
     }
 
+    public boolean isObjectOrNullKey() {
+        return isObjectKey() || isNullKey();
+    }
+
     public boolean isPrimitiveKey() {
         return keyOption() instanceof PrimitiveType;
     }
@@ -110,12 +114,10 @@ public class MethodContext {
 
     /** @return K or char..long, floating bits */
     public String keyUnwrappedType() {
-        if (isObjectKey()) {
+        if (isObjectKey() || isNullKey()) {
             return ObjectType.genericParamName(keyDim());
         } else if (isPrimitiveKey()) {
             return primitiveBitsType(keyOption());
-        } else if (isNullKey()) {
-            return "Object";
         } else {
             throw new IllegalStateException();
         }
@@ -222,6 +224,10 @@ public class MethodContext {
 
     public Option mapValueOption() {
         return getOption("value");
+    }
+
+    public boolean hasValues() {
+        return mapValueOption() != null;
     }
 
     public Option getOption(String dim) {

buildSrc/src/main/groovy/net/openhft/jpsg/collect/MethodGeneratingProcessor.java
@@ -65,6 +65,7 @@ public class MethodGeneratingProcessor extends TemplateProcessor {
                     put(Index.class);
                     put(Insert.class);
                     put(ValueIndex.class);
+                    put(RemoveAt.class);
 
                     // map query/update
                     put(ContainsEntry.class);
@@ -102,6 +103,7 @@ public class MethodGeneratingProcessor extends TemplateProcessor {
                             {
                                 put(HashBulkMethodGenerator.class);
                                 put(HashMapQueryUpdateMethodGenerator.class);
+                                put(RemoveAt.MethodGenerator.class);
                             }
 
                             private void put(Class<? extends MethodGenerator> c) {

buildSrc/src/main/groovy/net/openhft/jpsg/collect/algo/hash/HashBulkMethodGenerator.java
@@ -17,14 +17,14 @@
 package net.openhft.jpsg.collect.algo.hash;
 
 import net.openhft.jpsg.PrimitiveType;
-import net.openhft.jpsg.collect.*;
+import net.openhft.jpsg.collect.Method;
+import net.openhft.jpsg.collect.Permission;
 import net.openhft.jpsg.collect.bulk.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static net.openhft.jpsg.collect.algo.hash.HashMethodGeneratorCommons.free;
-import static net.openhft.jpsg.collect.algo.hash.HashMethodGeneratorCommons.removed;
+import static net.openhft.jpsg.collect.algo.hash.HashMethodGeneratorCommons.*;
 
 
 public class HashBulkMethodGenerator extends BulkMethodGenerator {
@@ -52,7 +52,7 @@ public class HashBulkMethodGenerator extends BulkMethodGenerator {
         }
         if (cxt.isIntegralKey()) {
             lines(cxt.keyType() + " free = freeValue;");
-            if (cxt.mutable()) {
+            if (possibleRemovedSlots(cxt)) {
                 lines(cxt.keyType() + " removed = removedValue;");
             }
         }
@@ -61,7 +61,7 @@ public class HashBulkMethodGenerator extends BulkMethodGenerator {
 
         method.rightBeforeLoop();
 
-        boolean splitLoops = cxt.mutable() && !cxt.isFloatingKey();
+        boolean splitLoops = possibleRemovedSlots(cxt) && !cxt.isFloatingKey();
         if (splitLoops) {
             lines("if (noRemoved()) {");
             indent();

buildSrc/src/main/groovy/net/openhft/jpsg/collect/algo/hash/HashCursorMethodGenerator.java
@@ -41,22 +41,24 @@ public class HashCursorMethodGenerator extends CursorMethodGenerator {
     public void generateConstructor() {
         commonConstructorOps(this, cxt, false);
         if (cxt.isObjectKey()) {
-            this.lines(
+            lines(
                     "// noinspection unchecked",
-                    "this.keys = (" + cxt.keyUnwrappedType() + "[]) set;"
+                    (needCapacityMask(cxt) ? cxt.keyType() + "[] keys = " : "") +
+                            "this.keys = (" + cxt.keyUnwrappedType() + "[]) set;"
             );
         } else {
-            this.lines("this.keys = set;");
+            lines((needCapacityMask(cxt) ? cxt.keyUnwrappedType() + "[] keys = " : "") +
+                    "this.keys = set;");
         }
-        if (!cxt.isKeyView()) {
+        if (needCapacityMask(cxt))
+            lines("capacityMask = keys.length - 1;");
+        if (!cxt.isKeyView())
             this.lines("vals = values;");
-        }
         if (cxt.isIntegralKey()) {
             this.lines(cxt.keyUnwrappedType() + " " + free(cxt) +
                     " = this." + free(cxt) + " = freeValue;");
-            if (cxt.mutable()) {
+            if (possibleRemovedSlots(cxt))
                 this.lines("this." + removed(cxt) + " = removedValue;");
-            }
         }
         lines("curKey = " + free(cxt) + ";");
     }
@@ -106,6 +108,11 @@ public class HashCursorMethodGenerator extends CursorMethodGenerator {
         ifBlock(isNotFree(cxt, "curKey"));
         checkModCount(this, cxt, false);
         lines("vals[index] = " + unwrapValue("value") + ";");
+        if (possibleArrayCopyOnRemove(cxt)) {
+            ifBlock("vals != values"); {
+                lines("values[index] = " + unwrapValue("value") + ";");
+            } blockEnd();
+        }
         endOfModCountCheck(this, cxt);
         endOfIllegalStateCheck(this, cxt);
     }
@@ -127,26 +134,60 @@ public class HashCursorMethodGenerator extends CursorMethodGenerator {
     @Override
     public void generateRemove() {
         permissions.add(Permission.REMOVE);
+        lines(cxt.keyUnwrappedRawType() + " curKey;");
+        String curKeyAssignment = "(curKey = this.curKey)";
         if (cxt.isIntegralKey()) {
             lines(cxt.keyType() + " " + free(cxt) + ";");
-            ifBlock("curKey != (" + free(cxt) +" = this." + free(cxt) + ")");
+            ifBlock(curKeyAssignment + " != (" + free(cxt) +" = this." + free(cxt) + ")");
         } else {
-            ifBlock(isNotFree(cxt, "curKey"));
+            ifBlock(isNotFree(cxt, curKeyAssignment));
         }
         ifBlock("expectedModCount++ == " + modCount());
+        // Local copy still holds the current key (could be used in shiftRemove())
+        lines("this.curKey = " + free(cxt) + ";");
+        if (isLHash(cxt)) {
+            shiftRemove();
+        } else {
+            tombstoneRemove();
+        }
+        endOfModCountCheck(this, cxt);
+        endOfIllegalStateCheck(this, cxt);
+    }
+
+    private void tombstoneRemove() {
         incrementModCount();
         if (cxt.isObjectValue())
             lines("int index;");
         String indexAssignment = cxt.isObjectValue() ? "index = this.index" : "index";
-        String keys = cxt.isObjectKey() ? "((Object[]) keys)" : "keys";
-        lines(keys + "[" + indexAssignment + "] = " + removed(cxt) + ";");
-        if (cxt.isObjectValue()) {
-            lines("vals[index] = null;");
-        }
+        eraseSlot(this, cxt, indexAssignment, "index");
         lines("postRemoveHook();");
-        lines("curKey = " + free(cxt) + ";");
-        endOfModCountCheck(this, cxt);
-        endOfIllegalStateCheck(this, cxt);
+    }
+
+    private void shiftRemove() {
+        lines("int index = this.index;");
+        new IterShiftRemove(this, cxt) {
+            @Override
+            String slotsToCopy() {
+                return "index";
+            }
+
+            @Override
+            void onInitialSlotSubstitution() {
+                // Step `index` back, because moveNext() starts from `index` - 1 and won't see
+                // the entry shifted to the slot at `index`.
+                // Update the local copy too, because this variable could be used
+                // later in the table copying (see IterShiftRemove.beforeShift()).
+                lines("this.index = ++index;");
+            }
+
+            @Override
+            String keyToRemoveFromTheOriginalTable() {
+                String key = "curKey";
+                if (cxt.isObjectKey())
+                    key = "(" + cxt.keyType() + ") " + key;
+                return key;
+            }
+        }.generate();
     }
 
     @Override

commit dc1aa0bf57fc303bfe02c861ac32644b3f44a35b

Fixed an awful bug in HashMapQueryUpdateMethodGenerator, mutable lhash maps and sets with object keys were broken. Thanks to Mikhail Vorontsov. This bug was introduced in dd96166.

lib/impl-generator/src/main/java/net/openhft/jpsg/collect/algo/hash/HashMapQueryUpdateMethodGenerator.java
@@ -464,13 +464,13 @@ public final class HashMapQueryUpdateMethodGenerator extends MapQueryUpdateMetho
                 elseBlock();
             }
             firstIndexFreeCheck("cur");
-            if (cxt.isObjectKey() && !cxt.mutable()) {
+            if (cxt.isObjectKey() && !possibleRemovedSlots(cxt)) {
                 ifBlock("keyEquals(" + unwrappedKey() + ", cur)");
                 generateOrGoToPresent(() -> {});
                 elseBlock();
             }
             innerInline();
-            if (cxt.isObjectKey() && !cxt.mutable())
+            if (cxt.isObjectKey() && !possibleRemovedSlots(cxt))
                 blockEnd();
             blockEnd();
             if (separateAbsentFreeSlot && !earlyAbsentLabel)

lib/impl/src/test/java/net/openhft/koloboke/collect/map/hash/ObjMapTest.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.openhft.koloboke.collect.map.hash;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ObjMapTest {
+
+    @Test
+    public void test() {
+        Map<Integer, Integer> map = HashObjObjMaps.newMutableMap(10);
+        int val1 = 1024*1024;
+        int val2 = 1024*1024 + 1;
+        int val3 = 1024*1024 + 2;
+        int val4 = 1024*1024 + 3;
+        map.put(val1, val1);
+        map.put(val2, val2);
+        map.put(val3, val3);
+        map.put(val4, val4);
+
+        assertEquals((Integer) val1, map.get(val1));
+        assertEquals((Integer) val2, map.get(val2));
+        assertEquals((Integer) val3, map.get(val3));
+        assertEquals((Integer) val4, map.get(val4));
+    }
+}